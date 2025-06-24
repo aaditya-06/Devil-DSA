9ab114976a32c5f1470b73ac389de6077ad8c8d8
Aditya <adityanehare571@gmail.com>
2025-06-25 00:41:28 +0530

    Day 3

--- Arrays/reverseArray.cpp
+++ Arrays/reverseArray.cpp
@@ -1,14 +1,50 @@
 #include <iostream>
 using namespace std;
 
+void print(int *arr, int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+void extraSpaceReverse(int *arr, int n)
+{
+    int copyarr[n];
+    for (int i = 0; i < n; i++)
+    {
+        copyarr[i] = arr[n - i - 1];
+    }
+
+    print(copyarr, n);
+}
+
+
+void inPlaceSwapReverse(int *arr, int n)
+{
+    int start = 0, end = n - 1;
+    while (start < end)
+    {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+
+    print(arr, n);
+}
+
 int main()
 {
-    int arr[8] = {2, 4, 6, 8, 10, 12, 14, 16};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int arr1[8] = {2, 4, 6, 8, 10, 12, 14, 16};
+    int arr2[8] = {2, 4, 6, 8, 10, 12, 14, 16};
 
-    for (int i = n; i > 0; i--)
-    {
-        cout << arr[i] << " ";
-    }
+    cout << "Reverse using extra space: ";
+    extraSpaceReverse(arr1, 8);
+
+    cout << "Reverse using in-place swap: ";
+    inPlaceSwapReverse(arr2, 8);
+
     return 0;
 }
